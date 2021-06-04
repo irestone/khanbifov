f6429ce72f3eeba6a9d56945ea989aea05966f21
Khan Bifov <khan.bifov@gmail.com>
2021-06-04 11:39:58 +0300

    add <Icon />

---
+++ components/Icon.tsx
@@ -0,0 +1,51 @@
+/** @jsxImportSource @emotion/react */
+
+import { FC, SVGProps, useMemo } from 'react'
+
+import twitter from '../public/icons/twitter.svg'
+import github from '../public/icons/github.svg'
+import linkedin from '../public/icons/linkedin.svg'
+import email from '../public/icons/email.svg'
+import location from '../public/icons/location.svg'
+import globe from '../public/icons/globe.svg'
+
+type TIconName = 'twitter' | 'github' | 'linkedin' | 'email' | 'location' | 'globe'
+
+const icons: { [name in TIconName]: FC<SVGProps<SVGSVGElement>> } = {
+  twitter,
+  github,
+  linkedin,
+  email,
+  location,
+  globe,
+}
+
+const Icon: FC<{ [name in TIconName]?: boolean } & SVGProps<SVGSVGElement>> = ({
+  location,
+  email,
+  twitter,
+  github,
+  linkedin,
+  globe,
+  ...props
+}) => {
+  const iconProps: { [name in TIconName]: boolean | undefined } = useMemo(
+    () => ({
+      location,
+      email,
+      twitter,
+      github,
+      linkedin,
+      globe,
+    }),
+    [location, email, twitter, github, linkedin, globe]
+  )
+  const name = useMemo(() => {
+    const [[name]] = Object.entries(iconProps).filter(([, v]) => Boolean(v))
+    return name as TIconName
+  }, [iconProps])
+  const Component = useMemo(() => icons[name], [name])
+  return Component ? <Component className={`icon ${name}`} {...props} /> : null
+}
+
+export default Icon
--- components/layout.tsx
+++ components/layout.tsx
@@ -12,6 +12,7 @@ import Head from 'next/head'
 import HomeIcon from '../public/icons/home.svg'
 import theme from '../styles/theme'
 import { TInfoData } from '../lib/api'
+import Icon from './Icon'
 
 // not too adaptive yet
 const AdaptiveContainer = s.div({
@@ -55,6 +56,7 @@ const Nav: FC<{ routeNames: string[] }> = ({ routeNames }) => {
       <NavLink href='/'>
         <HomeIcon css={{ fill: 'inherit', width: '1.1rem', height: '1.1rem' }} />
       </NavLink>
+      {/* todo: make a list (like in footer) */}
       {routeNames
         .map((routeName) => {
           return [
@@ -69,14 +71,12 @@ const Nav: FC<{ routeNames: string[] }> = ({ routeNames }) => {
   )
 }
 
-const IconLink: FC<{ href?: string; icon: string }> = ({ href, icon, children }) => {
-  return (
-    <a href={href} css={{ display: 'inline-flex', alignItems: 'center', cursor: 'pointer' }}>
-      <img src={`/icons/${icon}.svg`} css={{ width: '1rem', height: '1rem', margin: '0 .3rem' }} />
-      <span>{children}</span>
-    </a>
-  )
-}
+const IconLink = s.span({
+  display: 'inline-flex',
+  alignItems: 'center',
+  '.icon': { width: '1rem', height: '1rem', marginRight: '.3rem' },
+  a: { cursor: 'pointer', ':hover': { textDecoration: 'underline' } },
+})
 
 const Layout: FC<{ info: TInfoData }> = ({ info, children }) => {
   const { colors, fonts } = useTheme()
@@ -93,7 +93,7 @@ const Layout: FC<{ info: TInfoData }> = ({ info, children }) => {
 
   const mainJSX = (
     <main css={{ padding: '3.3rem', background: colors.skin1 }}>
-      <div css={{ border: `1px solid #515f56`, background: '#7a8a80' }}>{children}</div>
+      <div css={{ border: `1px solid #515f56`, background: '#555e5f' }}>{children}</div>
     </main>
   )
 
@@ -104,7 +104,7 @@ const Layout: FC<{ info: TInfoData }> = ({ info, children }) => {
           font: `400 2.5rem ${fonts.exo2}`,
           wordSpacing: '.2em',
           textAlign: 'right',
-          color: colors.white,
+          color: 'white',
         }}
       >
         Neo-Tokyo is about to <span css={{ letterSpacing: '0.17em' }}>E•X•P•L•O•D•E</span>
@@ -124,34 +124,47 @@ const Layout: FC<{ info: TInfoData }> = ({ info, children }) => {
         textTransform: 'uppercase',
         letterSpacing: '0.03em',
         color: colors.yellow1,
-        'a: hover': { textDecoration: 'underline' },
+        fill: colors.yellow1,
       }}
     >
       <div>
-        <Link href='/' locale={locale === 'en' ? 'ru' : 'en'}>
-          <a style={{ display: 'inline-flex', alignItems: 'center', cursor: 'pointer' }}>
-            <img
-              src={`/icons/globe.svg`}
-              css={{ width: '1rem', height: '1rem', margin: '0 .3rem' }}
-            />
-            <span>{locale === 'en' ? 'ru' : 'en'}</span>
-          </a>
-          {/* <IconLink icon='globe'>{locale === 'en' ? 'ru' : 'en'}</IconLink> */}
-        </Link>
-      </div>
-      <div>
-        <IconLink href={info.twitter} icon='twitter'>
-          {t('twitter')}
-        </IconLink>
-        {' / '}
-        <IconLink href={info.github} icon='github'>
-          {t('github')}
-        </IconLink>
-        {' / '}
-        <IconLink href={info.linkedin} icon='linkedin'>
-          {t('linkedin')}
+        <IconLink>
+          <Icon globe />
+          <Link href='/' locale={locale === 'en' ? 'ru' : 'en'}>
+            {locale === 'en' ? 'ru' : 'en'}
+          </Link>
         </IconLink>
       </div>
+      <ul
+        css={{
+          display: 'flex',
+          alignItems: 'center',
+          'li + li::before': {
+            content: '"/"',
+            display: 'inline-block',
+            margin: '0 .5em',
+          },
+        }}
+      >
+        <li>
+          <IconLink>
+            <Icon twitter />
+            <a href={info.twitter}>{t('twitter')}</a>
+          </IconLink>
+        </li>
+        <li>
+          <IconLink>
+            <Icon github />
+            <a href={info.github}>{t('github')}</a>
+          </IconLink>
+        </li>
+        <li>
+          <IconLink>
+            <Icon linkedin />
+            <a href={info.linkedin}>{t('linkedin')}</a>
+          </IconLink>
+        </li>
+      </ul>
     </div>
   )
 
